bdb9850e764da3113f18b2cf1118d7028c2733a2
maha <mmahaece072gmail.com>
2021-11-10 12:06:22 +0530

    Add project files.

---
+++ day15 hash table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace day15_hash_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // UC1
+
+            UC1_line_frequency hash1 = new UC1_line_frequency();
+            hash1.Freq();
+
+            //UC2
+
+            UC2_large_para hash2 = new UC2_large_para();
+            hash2.FreqLarge();
+
+            //UC3
+
+            UC3_Remove hash3 = new UC3_Remove();
+            hash3.removeWord("");
+        }
+    }
+    }
+
---
+++ day15 hash table/UC3_Remove.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day15_hash_table
+{
+    class UC3_Remove
+    {
+        public void removeWord(string word)
+        {
+            string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] arr = para.Split(" ");
+            int size = arr.Length;
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(size - 1);
+            for (int i = 0; i < size; i++)
+            {
+                hash.Add(arr[i], i.ToString());
+            }
+            for (int i = 0; i < size; i++)
+            {
+                hash.Add(arr[i], i.ToString());
+            }
+
+
+            hash.Remove(hash.Get(word));
+        }
+    }
+}
+
